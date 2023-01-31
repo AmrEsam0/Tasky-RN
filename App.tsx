91f0d7ec82b84b7c70dca084cce29adef497d7cb
Amr Essam <amr_esam@ymail.com>
2023-01-31 06:07:15 +0200

    got rid of expo

--- App.tsx
+++ App.tsx
@@ -1,32 +1,30 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
 
-import { Card } from "./components/Card";
-import { NumAPICard } from "./components/NumAPICard";
+import {LoginScreen} from './components/LoginScreen';
+import {NumAPICard} from './components/NumAPICard';
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<StatusBar style="inverted" />
-			<Card />
-			{/* <NumAPICard /> */}
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <LoginScreen />
+      {/* <NumAPICard /> */}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#2C2A57",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	mainHeader: {
-		alignSelf: "flex-start",
-		fontSize: 90,
-		flex: 0.2,
-		color: "#2C2A57",
-		marginBottom: 20,
-	},
+  container: {
+    flex: 1,
+    backgroundColor: '#2C2A57',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainHeader: {
+    alignSelf: 'flex-start',
+    fontSize: 90,
+    flex: 0.2,
+    color: '#2C2A57',
+    marginBottom: 20,
+  },
 });
