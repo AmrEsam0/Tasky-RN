b4cd2423fe61bb009489edd157534a978e975713
Amr Essam <amr_esam@ymail.com>
2023-02-11 17:15:49 +0200

    CusotmButton is full width now

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -16,7 +16,16 @@ export const CustomButton = ({
 }) => {
   return (
     <Button
-      icon={() => <Icon name={iconName} size={22} color={Colors.textDark} />}
+      icon={() => (
+        <Icon
+          name={iconName}
+          size={20}
+          color={Colors.textDark}
+          style={{
+            textAlignVertical: 'center',
+          }}
+        />
+      )}
       mode="contained"
       style={styles.button}
       labelStyle={styles.label}
@@ -30,16 +39,20 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: Colors.backgroundLighter,
     marginTop: '4%',
-    width: '46%',
+    width: '100%',
+    alignContent: 'center',
     alignSelf: 'center',
     borderRadius: 4,
     justifyContent: 'center',
     marginBottom: '2%',
+    height: 50,
   },
   label: {
-    fontSize: 20,
+    fontSize: 18,
+    textAlignVertical: 'center',
+    marginTop: '3.5%',
     alignSelf: 'center',
-    marginTop: '9%',
+    height: 50,
     fontFamily: Fonts.TextNormal,
     color: Colors.textDark,
   },
